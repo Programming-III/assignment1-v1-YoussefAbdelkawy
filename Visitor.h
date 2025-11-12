446f630e4ea15f6f9c6f987337174ec43e920bac
Youssef Abdelkawi <200792126+YoussefAbdelkawy@users.noreply.github.com>
2025-11-12 15:11:57 +0200

    Implement Visitor class in Visitor.h
    Added a Visitor class with properties and constructors.

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,30 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
-#include <string>
-using namespace std;
-
-//define visitor class here
-
-
-
+//
+// Created by yusef on 11/12/2025.
+//
+#include <iostream>
+#include<string>
+using namespace std;//
+#ifndef PROG1ASSIGMENT_VISITOR_H
+#define PROG1ASSIGMENT_VISITOR_H
 
 
+class Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
+public:
+    Visitor() {
+        this->visitorName = "";
+        this->ticketsBought = 0;
+    }
+    void displayInfo();
+    Visitor(string visitorName, int ticketsBought) {
+        this->visitorName = visitorName;
+        this->ticketsBought = ticketsBought;
+    }
+    ~Visitor() {}
+
+};
 
 
-#endif
+#endif //PROG1ASSIGMENT_VISITOR_H
