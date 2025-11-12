48f7886f83644e37fd756cf9b4bf1a6a9a0d8330
Youssef Abdelkawi <200792126+YoussefAbdelkawy@users.noreply.github.com>
2025-11-12 15:11:17 +0200

    Define Mammal class inheriting from Animal
    Added Mammal class definition with constructors and destructor.

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,29 @@
-#ifndef REPTILE_H
-#define REPTILE_H
+#include <iostream>
+#include<string>
 
 #include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
+using namespace std;//
+// Created by yusef on 11/12/2025.
+//
+#include "Animal.h"
+#ifndef PROG1ASSIGMENT_MAMMAL_H
+#define PROG1ASSIGMENT_MAMMAL_H
 
 
+class Mammal : public Animal{
+private:
+    string furColor;
+public:
+    Mammal() {
+        this->furColor="";
+    }
+    Mammal(string name,int age,bool isHugry,string furColor): Animal( name, age, isHugry) {
+        this->furColor = furColor;
+    }
+    ~Mammal() {
 
-#endif
+    }
+};
+
+
+#endif //PROG1ASSIGMENT_MAMMAL_H
