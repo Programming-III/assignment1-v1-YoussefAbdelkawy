e1a7046b75036ef06110185517948fc0f4138169
Youssef Abdelkawi <200792126+YoussefAbdelkawy@users.noreply.github.com>
2025-11-12 15:07:28 +0200

    Update Animal class with new constructor and includes
    Refactored Animal class definition and added constructors.

--- Animal.h
+++ Animal.h
@@ -1,11 +1,39 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
+#include <iostream>
+#include<string>
+using namespace std;//
+// Created by yusef on 11/12/2025.
+//
 
-#include <string>
-using namespace std;
-
-//define animal class here 
+#ifndef PROG1ASSIGMENT_ANIMAL_H
+#define PROG1ASSIGMENT_ANIMAL_H
 
 
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
+public:
+    Animal();
+    Animal(string name,int age,bool isHugry) {
+        this->name = name;
+        this->age = age;
+        this->isHungry = isHugry;
+    }
+    void display();
+    void feed();
+    string getName();
+    int getAge();
+    void setName(string);
+    void setAge(int);
+    ~Animal(){}
 
-#endif
+
+
+
+
+
+};
+
+
+#endif //PROG1ASSIGMENT_ANIMAL_H
