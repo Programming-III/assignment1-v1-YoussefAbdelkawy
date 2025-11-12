d40ff9cef25d3f5ca7038852bbab584b07c5c55d
Youssef Abdelkawi <200792126+YoussefAbdelkawy@users.noreply.github.com>
2025-11-12 15:10:13 +0200

    Implement Enclosure class with animal management
    Added a new enclosure class with methods to add and display animals.

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,41 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+//
+// Created by yusef on 11/12/2025.
+//
+#include <iostream>
+using namespace std;//
+// Created by yusef on 11/12/2025.
+//
 
 #include "Animal.h"
-
-//define enclosure class here 
-
-
+#ifndef PROG1ASSIGMENT_ENCLOSURE_H
+#define PROG1ASSIGMENT_ENCLOSURE_H
 
 
+class Enclosure {
+    private:
+    Animal* animal;
+    int capacity;
+    int currentCount;
+    public:
+    void addAnimal(Animal* a);
+    void displayAnimals();
+public:
+    Enclosure() {
+        this->animal=nullptr;
+        this->capacity=10;
+        this->currentCount=0;
+    };
+
+    Enclosure(Animal* animal,int capacity,int currentCount) {
+        this->animal= animal;
+        this->capacity = capacity;
+        this->currentCount = currentCount;
+    }
+
+    ~Enclosure() {
+        delete [] animal;
+    }
+};
 
 
-
-
-#endif
+#endif //PROG1ASSIGMENT_ENCLOSURE_H
