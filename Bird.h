bb8e35c210dbb6e3f5f3c1c847da54e800ddcc39
Youssef Abdelkawi <200792126+YoussefAbdelkawy@users.noreply.github.com>
2025-11-12 15:09:43 +0200

    Implement Bird class with wingSpan attribute
    Added Bird class that inherits from Animal with wingSpan attribute.

--- Bird.h
+++ Bird.h
@@ -1,14 +1,27 @@
-#ifndef BIRD_H
-#define BIRD_H
-
+//
+// Created by yusef on 11/12/2025.
+//#include <iostream>
+#include<string>
+using namespace std;//
 #include "Animal.h"
-
-//define class bird here 
-
-
+#ifndef PROG1ASSIGMENT_BIRD_H
+#define PROG1ASSIGMENT_BIRD_H
 
 
+class Bird : public Animal{
+private:
+    float wingSpan;
+public:
+    Bird() {
+        this->wingSpan=0.0;
+    }
+    Bird(string name,int age,bool isHugry,float wingSpan): Animal( name, age, isHugry) {
+        this->wingSpan = wingSpan;
+    }
+    ~Bird() {
+
+    }
+};
 
 
-
-#endif
+#endif //PROG1ASSIGMENT_BIRD_H
